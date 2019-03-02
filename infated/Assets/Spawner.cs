c58759bdd959f9198c9ec99a2c81f1a93bb85713
ugursoy <sadikugursoy@gmail.com>
2019-03-02 16:36:04 +0300

    Reorganised folder hierarchy. Removed old animation sprites

---
+++ infated/Assets/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject _GameObject;
+    public Vector3 _position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_GameObject, _position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
