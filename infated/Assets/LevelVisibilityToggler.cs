684f818f8fc3682fdaf1f3f37d2539aadea3b285
ugursoy <sadikugursoy@gmail.com>
2019-04-26 21:56:24 +0300

    Updated level 0. Added cave and its props.

---
+++ infated/Assets/LevelVisibilityToggler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelVisibilityToggler : MonoBehaviour
+{
+    public GameObject _Object;
+    private Animator _Animator;
+    private Renderer _Renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Animator = _Object.GetComponent<Animator>();
+        _Object.GetComponent<MeshRenderer>().sortingOrder = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        fadeIn();
+    }
+
+    private void OnTriggerExit2D(Collider2D other){
+       fadeOut();
+    }
+
+    private void fadeIn(){
+        _Animator.Play("CaveCoverFadeIn");
+    }
+    private void fadeOut(){
+        _Animator.Play("CaveCoverFadeOut");
+    }
+}
